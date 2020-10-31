adcd1dfc393774653d2a20d5f3b4f3692418d02d
stirumala <stirumala@micron.com>
2020-10-31 19:52:18 +0530

    init commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('webcastnote app is running!');
+    expect(page.getTitleText()).toEqual('itrackon app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'webcastnote'`, () => {
+  it(`should have as title 'itrackon'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('webcastnote');
+    expect(app.title).toEqual('itrackon');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('webcastnote app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('itrackon app is running!');
   });
 });
